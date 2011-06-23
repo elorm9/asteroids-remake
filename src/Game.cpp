4596f166fe30dd2e26da316393102b1e660e5255
elorm9 <elorm9@fb6d4d29-4375-b82c-21c5-7c8e682fd47b>
2011-06-23 16:24:16 +0000

    Quick Commit

--- src/Game.cpp
+++ src/Game.cpp
@@ -11,18 +11,15 @@
 using namespace std;
 
 int main() {
-	sf::RenderWindow Game(sf::VideoMode(1000,600,32), "Game");
+	sf::RenderWindow Game(sf::VideoMode(800,600,32), "Game");
 	Game.UseVerticalSync(true);
-	Game.EnableKeyRepeat(false);
 	Player user(300,300);
 
-	const sf::Input& input = Game.GetInput();
 	sf::Event event;
 
 
 	while(Game.IsOpened()){
-		user.handle_input(input);
-		user.update(Game);
+		user.handle_input(Game);
 		Game.Clear(sf::Color(0, 0, 0, 0));
 		Game.Draw(user.getEntity().getSprite());
 		Game.Display();
